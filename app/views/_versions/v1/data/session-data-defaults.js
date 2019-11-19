0c1b9581042ac930b17dd6c71edc89df1b6bfee5
Pete Blakemore <contact@peteblakemore.co.uk>
2019-11-19 10:57:49 +0000

    Fixed broken link

---
+++ app/views/_versions/v1/data/session-data-defaults.js
@@ -0,0 +1,24 @@
+/*
+============================================================================
+Vacancies JSON
+============================================================================
+*/
+const vacancies = require('./vacancies.json')
+
+/*
+============================================================================
+Default data
+============================================================================
+*/
+
+module.exports = {
+
+  "employer-name": "Plumb Perfect Ltd",
+  "recruitment-provider-scenario": "recruitment_no-provider.html",
+  "recruitment-employer-scenario": "recruitment_employer-no-vacancy.html",
+  "recruitment-option": "split",
+  "recruitment-heading": "Recruitment",
+  "provider-name": "Coventry College",
+  "vacancies": vacancies,
+  "vacancy-filter": "all"
+}
